7bcaad6ead1850b6b5c0849f4ee9ebe5d5ead92b
Joey <woqiaoxun@163.com>
2019-08-27 21:24:49 +0800

    add log

--- src/main/java/com/qiao/controller/HelloController.java
+++ src/main/java/com/qiao/controller/HelloController.java
@@ -1,6 +1,8 @@
 package com.qiao.controller;
 
 import com.qiao.domain.Person;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,11 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    private Logger logger = LoggerFactory.getLogger(HelloController.class);
+
     @Autowired
     private Person person;
 
     @RequestMapping("hello")
     public String hello() {
+        logger.info("person: " + person);
         System.out.println(person);
         return person.toString();
     }
