32769d25e317dfcda3625bfbd9ebaac9fd40cb0d
Joey <woqiaoxun@163.com>
2019-11-02 23:31:16 +0800

    add cache

--- cache-study/src/main/java/com/study/cache/controller/CacheController.java
+++ cache-study/src/main/java/com/study/cache/controller/CacheController.java
@@ -4,6 +4,7 @@ import com.study.cache.domain.Booking;
 import com.study.cache.service.BookingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,9 +17,19 @@ public class CacheController {
     @Autowired
     private BookingService bookingService;
 
-    @GetMapping("/listBookings")
-    public List<Booking> listBookings() {
-        return bookingService.listBookings();
+    @GetMapping("/listBookings/{id}")
+    public List<Booking> listBookings(@PathVariable Long id) {
+        return bookingService.listBookings1(id, "test");
+    }
+
+    @GetMapping("/getBookingById/{id}")
+    public Booking getBookingById(@PathVariable Long id) {
+        return bookingService.getBookingById(id);
+    }
+
+    @GetMapping("/updateBooking/{id}")
+    public Booking updateBooking(@PathVariable Long id, Double duration, String remark) {
+        return bookingService.updateBookingById(id, duration, remark);
     }
 
 }
--- cache-study/src/main/java/com/study/cache/service/BookingService.java
+++ cache-study/src/main/java/com/study/cache/service/BookingService.java
@@ -3,22 +3,81 @@ package com.study.cache.service;
 import com.study.cache.domain.Booking;
 import com.study.cache.mapper.BookingMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.CacheConfig;
-import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.*;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-//@CacheConfig(cacheNames = "booking")
+@CacheConfig(cacheNames = "booking")
 public class BookingService {
 
     @Autowired
     private BookingMapper bookingMapper;
 
-    @Cacheable("bookings")
-    public List<Booking> listBookings() {
+    @Cacheable(cacheNames = {"bookings", "bookings2"}, key = "#room.methodName+'['+#p0+']'")
+    public List<Booking> listBookings(Long id, String name) {
         return bookingMapper.listAllBooings();
     }
+
+    @Cacheable(cacheNames = {"bookings", "bookings2"}, keyGenerator = "myKeyGenerator")
+    public List<Booking> listBookings1(Long id, String name) {
+        return bookingMapper.listAllBooings();
+    }
+
+    @Cacheable(cacheNames = {"bookings", "bookings2"}, keyGenerator = "myKeyGenerator", condition = "#a0>1")
+    public List<Booking> listBookings2(Long id, String name) {
+        return bookingMapper.listAllBooings();
+    }
+
+
+    @Cacheable(cacheNames = {"bookings", "bookings2"}, key = "#a0", unless = "#id==1")
+    public List<Booking> listBookings3(Long id, String name) {
+        return bookingMapper.listAllBooings();
+    }
+
+    @Cacheable(cacheNames = "bookings", key = "#id")
+    public Booking getBookingById(Long id) {
+        System.out.println("getBookingById = " + id);
+        Booking booking = new Booking();
+        booking.setId(id);
+        booking.setDuration(11.2);
+        booking.setRemark("test " + id);
+        return booking;
+    }
+
+    @CachePut(cacheNames = "bookings", key = "#result.id")
+    public Booking updateBookingById(Long id, Double duration, String remark) {
+        System.out.println("getBookingById = " + id + ", Double = " + duration + ", remark = " + remark);
+        Booking booking = new Booking();
+        booking.setId(id);
+        booking.setDuration(duration);
+        booking.setRemark(remark);
+        return booking;
+    }
+
+    @CacheEvict(cacheNames = "bookings", key = "#id", allEntries = true)
+    public void deleteBookingById(Long id) {
+        System.out.println("删除了 " + id);
+    }
+
+    @Caching(
+            cacheable = {
+                    @Cacheable(cacheNames = "bookings", key = "#a0"/** #p0, #id, #root.args[0] */)
+            },
+            put = {
+                    @CachePut(cacheNames = "bookings", key = "#result.duration"),
+                    @CachePut(cacheNames = "bookings", key = "#result.remark")
+            }
+    )
+    public Booking getBookingById2(Long id) {
+        System.out.println("getBookingById = " + id);
+        Booking booking = new Booking();
+        booking.setId(id);
+        booking.setDuration(11.2);
+        booking.setRemark("test " + id);
+        return booking;
+    }
+
 
 }
